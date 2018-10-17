e6f90de87641d32d6138a055071cc79eb3df0353
Eric Franco <france2@rpi.edu>
2018-10-17 03:40:02 -0400

    Spawn from four directions offscreen

--- js/spawn.js
+++ js/spawn.js
@@ -39,11 +39,11 @@ Spawn.prototype.update = function() {
 	if (this.game.input.keyboard.isDown(Phaser.KeyCode.A)) {
 		console.log(this.enemies.countLiving());
 	}
-	/*if(this.enemies.total == 0) {
-		for(var i = 0; i < 4; i++) {
-			this.spawnOffscreen(i);
+	if(this.enemies.total == 0) {
+		for(var i = 0; i < 20; i++) {
+			this.spawnOffscreen(i % 4);
 		}
-	}*/
+	}
 	
 	//this.despawnEnemies();
 }
